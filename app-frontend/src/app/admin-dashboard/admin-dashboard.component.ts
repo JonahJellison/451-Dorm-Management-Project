d9374a8db830e2876171f7e0db6d10e8bfc44c56
NelsonGoosman <nelsongoosman@gmail.com>
2025-03-28 13:54:30 -0700

    frontend work + backend models

--- app-frontend/src/app/admin-dashboard/admin-dashboard.component.ts
+++ app-frontend/src/app/admin-dashboard/admin-dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -7,6 +7,63 @@ import { Component } from '@angular/core';
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.css'
 })
-export class AdminDashboardComponent {
+export class AdminDashboardComponent implements OnInit {
 
+  totalStudents: number = 0;
+  occupancyRate: number = 0;
+  pendingRequests: number = 0;
+  matinanceRequests: number = 0;
+  numberOfRooms: number = 0;
+  occupiedRooms: number = 0;
+
+  dashboadView: boolean = true;
+  manageRoomView: boolean = false;
+  studentsView: boolean = false;
+
+  ngOnInit(): void {
+    // Initialize component data here
+
+  }
+
+
+  setDashboardView() {
+    this.dashboadView = true;
+    this.manageRoomView = false;
+    this.studentsView = false;
+  }
+
+  setManageRoomView() {
+    this.dashboadView = false;
+    this.manageRoomView = true;
+    this.studentsView = false;
+  }
+
+  setStudentsView() {
+    this.dashboadView = false;
+    this.manageRoomView = false;
+    this.studentsView = true;
+  }
+}
+
+interface RecentBooking{
+  studentInfo: Student
+  roomNumber: number;
+  checkInDate: Date;
+  checkOutDate: Date;
+  approvalStatus: boolean;
+  building: string;
+}
+
+interface Room {
+  roomNumber: number;
+  building: string;
+  occupancy: number;
+  status: string;
+  maintenance: boolean;
+  studentId: string;
+}
+
+interface Student {
+  studentId: string;
+  studentName: string;
 }
--- app-frontend/src/app/dorm-bookings/dorm-bookings.component.ts
+++ app-frontend/src/app/dorm-bookings/dorm-bookings.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dorm-bookings',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './dorm-bookings.component.html',
   styleUrl: './dorm-bookings.component.css'
 })
 export class DormBookingsComponent {
 
+selectedBuilding: string = '';
+selectedRoomType: string = '';
+selectedPriceRange: string = '';
+
+applyFilters() {
+  // Implement your filtering logic here
+  console.log('Filtering with:', {
+    building: this.selectedBuilding,
+    roomType: this.selectedRoomType,
+    priceRange: this.selectedPriceRange
+  });
+  
+  // Fetch or filter rooms based on selections
+}
 }
